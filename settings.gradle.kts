06fcdf7bff117ab9ead8765bb65fb8aadd7ef0fb
TomekoYT <tomekoyt@interia.pl>
2026-04-09 22:02:56 +0200

    update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,32 +1,32 @@
 pluginManagement {
-	repositories {
-		mavenCentral()
-		gradlePluginPortal()
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
 
-		maven("https://maven.fabricmc.net")
-		maven("https://jitpack.io/")
-		maven("https://maven.architectury.dev")
-		maven("https://maven.kikugie.dev/snapshots")
-		maven("https://maven.kikugie.dev/releases")
-	}
+        maven("https://maven.fabricmc.net")
+        maven("https://jitpack.io/")
+        maven("https://maven.architectury.dev")
+        maven("https://maven.kikugie.dev/snapshots")
+        maven("https://maven.kikugie.dev/releases")
+    }
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version providers.gradleProperty("stonecutter_version")
+    id("dev.kikugie.stonecutter") version providers.gradleProperty("stonecutter_version")
 }
 
 stonecutter {
-	create(rootProject) {
-		versions("1.21.9")
-		version("26.1").buildscript("unobfuscated.gradle.kts")
-		vcsVersion="1.21.9"
-	}
+    create(rootProject) {
+        versions("1.21.9")
+        version("26.1").buildscript("unobfuscated.gradle.kts")
+        vcsVersion = "1.21.9"
+    }
 }
 
 dependencyResolutionManagement {
-	versionCatalogs {
-		create("libs")
-	}
+    versionCatalogs {
+        create("libs")
+    }
 }
 
 rootProject.name = "ScreenshotMessageEnhancer"
