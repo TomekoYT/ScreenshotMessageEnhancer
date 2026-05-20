a8697b8c57ec6b080fdf2b9781a4c6ab460ffca8
TomekoYT <tomekoyt@interia.pl>
2026-05-20 11:44:56 +0200

    add bloom replacements

--- src/main/java/tomeko/screenshotmessageenhancer/utils/Constants.java
+++ src/main/java/tomeko/screenshotmessageenhancer/utils/Constants.java
@@ -1,8 +1,8 @@
 package tomeko.screenshotmessageenhancer.utils;
 
 public class Constants {
-    public static final String MOD_NAME = "Screenshot Message Enhancer";
-    public static final String MOD_ID = "screenshotmessageenhancer";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String MOD_ID = "@MOD_ID@";
 
     public static final String SCREENSHOT_COPY_COMMAND = "smescreenshotcopy";
     public static final String SCREENSHOT_DELETE_COMMAND = "smescreenshotdelete";
--- unobfuscated.gradle.kts
+++ unobfuscated.gradle.kts
@@ -16,6 +16,7 @@ val java_objective_c_bridge_version: String by project
 
 plugins {
     id("net.fabricmc.fabric-loom") version "1.16-SNAPSHOT"
+    id("dev.deftu.gradle.bloom") version "0.2.0"
 }
 
 base {
@@ -78,6 +79,11 @@ tasks.processResources {
     filesMatching("$mod_id.mixins.json") {
         expand(mixinProps)
     }
+}
+
+bloom {
+    replacement("@MOD_NAME@", mod_name)
+    replacement("@MOD_ID@", mod_id)
 }
 
 tasks.withType<JavaCompile>().configureEach {
